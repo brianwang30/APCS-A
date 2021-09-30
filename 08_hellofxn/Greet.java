ba3794f3f05f24be9fd9688d00936a2318de7894
brianwang30 <bwang30@stuy.edu>
2021-09-30 12:29:21 -0400

    initial setup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -3,6 +3,20 @@
 //HW08 - Greetings v2
 //2021-09-30
 
+/*
+DISCOVERIES
+
+Actually used nano for once. 
+Also figured out problems plaguing me when trying to commit and push stuff to git. 
+Did so by reading internet stranger texts. 
+*/
+
+/*
+UNRESOLVED QUESTIONS
+
+HOW DO I MOO? SERIOUSLY!
+*/
+
 public class Greet{
   public static void main(String[] args){
     greet("dude");
