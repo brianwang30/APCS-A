ecf33a6517c3a60a9d2488ab0844885ee865ece9
brianwang30 <bwang30@stuy.edu>
2021-10-01 12:17:13 -0400

    initial setup

---
+++ 09_bigsib/bigsib.java
@@ -0,0 +1,18 @@
+//Anything Goes:
+//Brian (Dolphin)
+//William (CS50DDB)
+//Brianna (Pepe)
+//APCS
+//HW09 - BigSibs
+//2021-10-01
+
+//Discoveries and Questions in Greet.java in 09_BigSibs. Probably.
+
+public class BigSib {
+        public static String greet(String name){
+                return "Hey there, " + name + ", how do you moo?";
+        }
+
+}
+
+
