6d9ce41cbf004f69ae77343d8b2fca1ca19f7de9
brianwang30 <bwang30@stuy.edu>
2021-10-01 12:11:29 -0400

    initial setup

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,27 @@
+//Anything Goes:
+//Brian (Dolphin)
+//William (CS50DDB)
+//Brianna (Pepe)
+//APCS
+//HW09 - BigSibs
+//2021-10-01
+
+/*
+DISCOVERIES
+
+Moving/Renaming files is useful
+*/
+
+/*
+UNRESOLVED QUESTIONS
+
+*/
+
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
