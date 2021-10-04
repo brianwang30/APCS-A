185184178dd348554fa1f052b5b8b37b5b105b0b
unknown <bwang30@stuy.edu>
2021-10-04 16:57:55 -0400

    initial setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,17 @@
+//Anything Goes:
+//Brian (Dolphin)
+//William (CS50DDB)
+//Brianna (Pepe)
+//APCS
+//HW10 - BigSibs
+//2021-10-04
+
+//Learned a few more error codes pertaining to some problems I had. 
+//Not much else in the terms of questions or discoveries, we barely covered anything. 
+
+public class BigSib {
+        public static String greet(String name){
+                 return "Hey there, " + name + ", how do you moo?";
+        }
+
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,17 @@
+//Anything Goes:
+//Brian (Dolphin)
+//William (CS50DDB)
+//Brianna (Pepe)
+//APCS
+//HW10 - BigSibs
+//2021-10-04
+
+
+public class Greet {
+    public static void main( String[] args ) {
+        System.out.println(BigSib.greet("Flim"));
+        System.out.println(BigSib.greet("Flam"));
+        System.out.println(BigSib.greet("Kazaam"));
+    }
+
+}
