e33f9a72ff2178ca2baea46a91ab87245df9df19
unknown <bwang30@stuy.edu>
2021-10-05 17:24:31 -0400

    Initial setup

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,18 @@
+
+
+public class BigSib {
+
+
+	public static String helloMsg;
+	public static void setHelloMsg(String message){
+		helloMsg = message;
+	}
+
+
+        public static String greet(String name){ 
+		return helloMsg + " " + name + "\nSalutations Dr. Spaceman\nHey ya Kong Fooey\nSup mom";
+        }
+
+
+
+}
---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,30 @@
+//Team Name Placeholder
+//Brian (Dolphin)
+//WHO IS MY TEAMMATE
+//APCS
+//HW12 - BigSibs
+//2021-10-05
+
+/* 
+Disco 
+*/ 
+/*
+ QCC What is a Kong Fooey?
+AND WHO IS MY PARTNER?
+I'M SORRY THERE WASN'T A LOT OF TIME
+I know he got my name hopefully he messages me
+ */
+
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
