4327ff3029b388676e9c6dfa48d32fc7cb559224
unknown <bwang30@stuy.edu>
2021-12-05 17:34:25 -0500

    update

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -7,13 +7,14 @@
 /*
 DISCOVERIES
    0. Functionality of compareTo being able to tell if the value is greater or smaller, compared to just equals.
+
 UNRESOLVED QUESTIONS
    0. Would it not be best to implement some simplifies inside of operations?
 */
 
 public class Rational {
-    protected int p;
-    protected int q;
+    private int p;
+    private int q;
 
     public Rational() {
         p = 0;
@@ -39,31 +40,25 @@ public class Rational {
         return p + "/" + q;
     }
 
-    public static int GCD( Rational r ) {
-    boolean loop = true;
-    int a = r.p;
-    int b = r.q;
-    while ( loop ) {
-      if (a > b ) {
-        a = a - b;
-      }
-      else {
+    public static int gcd(int a, int b) {
         if (a == b) {
-          loop = false;
+            return a;
         }
-        else {
-          b = b - a;
+        if (a < b) {
+            return gcd(a, b - a);
+        } else {
+            return gcd(a - b, b);
         }
-      }
     }
 
-      return a;
+    public int gcd() {
+        return gcd(this.p, this.q);
     }
 
     public void reduce(){
-      int gcd = GCD(this);
-      p = p / gcd;
-      q = q / gcd;
+        int _gcd = this.gcd();
+        p = p / _gcd;
+        q = q / _gcd;
     }
 
     public void add(Rational r){
@@ -83,8 +78,7 @@ public class Rational {
 
     public void divide(Rational r) {
         if (r.p == 0) {
-            // throw a divide by zero error
-            int i = 1 / 0;
+            System.out.println("cannot divide by zero, values unchanged");
         } else {
             p = p * r.q;
             q = q * r.p;
@@ -97,61 +91,22 @@ public class Rational {
     }
 
     public static void main(String[] args) {
-        Rational r0 = new Rational(10, 0);
-        System.out.println(r0);
-        System.out.println("=============");
-
-        Rational r1 = new Rational(10, 5);
-        Rational r2 = new Rational(3, 6);
-
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r1.floatValue());
-        r1.multiply(r2);
-        System.out.println(r1);
-        r1.divide(r2);
-        System.out.println(r1);
-
-        System.out.println("=============");
-        /*
-        Rational r3 = new Rational(2, 3);
-        Rational r4 = new Rational();
-
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println(r3.floatValue());
-        r3.multiply(r4);
-        System.out.println(r3);
-        r3.divide(r4);
-        System.out.println(r3);
-        */
-        //System.out.println(GCD(200, 100));
-        Rational r3 = new Rational(10, 5);
-        Rational r4 = new Rational(3, 6);
-        System.out.println(r3);
-        System.out.println(r4);
-        r3.reduce();
-        r4.reduce();
-        System.out.println(r3);
-        System.out.println(r4);
-        r3.add(r4);
-        System.out.println(r3);
-        r3.subtract(r4);
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println("Comparisons: ");
-        System.out.println(r3.compareTo(r3));
-        System.out.println(r3.compareTo(r4));
-        System.out.println(r4.compareTo(r3));
-
-        //===============================
-        System.out.println("\n\n");
         Rational r = new Rational(2,3); //Stores the rational number 2/3
         Rational s = new Rational(1,2); //Stores the rational number 1/2
-        Rational t = new Rational(4,18); //Stores the rational number 4/18
+        System.out.println("r: " + r + "\t\t\t\tshould be 2/3");
+        System.out.println("s: " + s + "\t\t\t\tshould be 1/2");
         r.add(s);  //Adds r to s, changes r to 7/6.  s remains 1/2
-        System.out.println(r);
+        System.out.println("r after r adding s: " + r + "\t\tshould be 7/6");
+        System.out.println("s after r adding s: " + s + "\t\tshould be 1/2");
+        System.out.println("==================================================");
+
+        Rational t = new Rational(4,18); //Stores the rational number 4/18
+        Rational u = new Rational(4,18); //Stores the rational number 4/18
+        System.out.println("t: " + t + "\t\t\t\tshould be 4/18");
+        System.out.println("u: " + u + "\t\t\t\tshould be 4/18");
         t.reduce(); //Changes t to 2/9
-        System.out.println(t);
-      }
+        System.out.println("t after reducing: " + t + "\t\tshould be 2/9");
+        System.out.println("t compared to u: " + t.compareTo(u) + "\t\tshould be zero");
+        System.out.println("t compared to r: " + t.compareTo(r) + "\t\tshould be negative");
+    }
 }
