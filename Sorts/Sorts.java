78301ad7f27d23f599ba2aeda0af5421adbd4c53
brianwang30 <bwang30@stuy.edu>
2022-01-06 12:22:23 -0500

    fix bits

--- Sorts/Sorts.java
+++ Sorts/Sorts.java
@@ -1,12 +1,27 @@
 import java.util.ArrayList;
 
 public class Sorts{
-  public static void main(String[] i'llsortyourfaceout){
+  public static void bubble(ArrayList<Comparable> unsorted){
+    BubbleSort.bubbleSortV(unsorted);
+  }
+
+  public static void insertion(ArrayList<Comparable> unsorted){
+    InsertionSort.insertionSortV(unsorted);
+  }
+
+  public static void selection(ArrayList<Comparable> unsorted){
+    SelectionSort.selectionSortV(unsorted);
+  }
+
+  public static void main(String[] illsortyourfaceout){
     ArrayList<Integer> test = new ArrayList<Integer>();
     for(int i = 0 ; i < 15 ; i += 1){
       test.add(math.random()*100);
     }
     System.out.println(test);
-    System.out.println()
+    Sorts.bubble(test);
+    //Sorts.insertion(test);
+    //Sorts.selection(test);
+    System.out.println(test);
   }
 }
